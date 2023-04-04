b1346e90234909d6cd4167ceccb3c9005661b11e
adamoutler@gmail.com <adamoutler@gmail.com>
2023-04-05 01:38:57 +0300

    just press F5 to start debugging

--- .vscode/helpers/link-code-to-system.sh
+++ .vscode/helpers/link-code-to-system.sh
@@ -6,12 +6,23 @@ fi
 rw
 loaded=false
 kvmd_location=/usr/lib/python3.10/site-packages/kvmd
+usrsharekvmd_location=/usr/share/kvmd/
 test -L "${kvmd_location}" && loaded=true
 if ! ${loaded}; then
-    source_location="$PWD"/../kvmd/kvmd/
-    rm -Rf ${kvmd_location}
-    ln -s ${source_location} "${kvmd_location}"
+    source_location="$PWD"/../kvmd
+    sudo rm -Rf ${kvmd_location}
+    ln -s ${source_location}/kvmd "${kvmd_location}"
     echo "KVMD replaced with source code"
+    rm -Rf ${usrsharekvmd_location}/web
+    ln -s ${source_location}/web "${usrsharekvmd_location}/web"
+    echo "Web replaced with source code"
+    rm -Rf ${usrsharekvmd_location}/extras
+    ln -s ${source_location}/extras "${usrsharekvmd_location}/extras" 
+    echo "Extras replaced with source code"
+    rm -Rf ${usrsharekvmd_location}/hid
+    ln -s ${source_location}/hid "${usrsharekvmd_location}/hid" 
+    echo "HID replaced with source code"
+    
 fi
 sudo chmod -R ag+w /usr/share/kvmd
 sudo chmod -R ag+w /etc/kvmd
