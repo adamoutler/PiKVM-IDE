6b1a41c7dc910d7367b523e912be8a6f7b438246
adamoutler <adamoutler@gmail.com>
2023-04-24 02:34:02 +0300

    Fix pug build appearing unresponsive at the end.

--- .vscode/helpers/pug/build-pug.sh
+++ .vscode/helpers/pug/build-pug.sh
@@ -1,7 +1,7 @@
 sudo bash .vscode/helpers/link-code-to-system.sh;
 if [ ! $(which pug) ]; then
 	sudo pacman -Syu npm;
-	sudo npm install pug htmlhint @babel/eslint-parser -g;
+	sudo npm install pug htmlhint @babel/eslint-parser eslint -g;
 	sudo npm install pug-cli -g;
 fi
 cd ../kvmd
@@ -10,5 +10,7 @@ pug --pretty web/login/index.pug -o web/login;
 pug --pretty web/kvm/index.pug -o web/kvm;
 pug --pretty web/ipmi/index.pug -o web/ipmi;
 pug --pretty web/vnc/index.pug -o web/vnc;
+echo "restarting kvmd service";
 sudo systemctl restart kvmd;
+echo "done building site.";
 cd -
