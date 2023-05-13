4d5b978260e5df6605da8271e15f5a46bae091a4
= <=>
2023-05-13 22:36:00 +0300

    Add types-PyYAML to link code to system

--- .vscode/helpers/link-code-to-system.sh
+++ .vscode/helpers/link-code-to-system.sh
@@ -9,7 +9,7 @@ kvmd_location=/usr/lib/python3.10/site-packages/kvmd
 usrsharekvmd_location=/usr/share/kvmd/
 test -L "${kvmd_location}" && loaded=true
 if ! ${loaded}; then
-    pip install debugpy aiohttp_basicauth asyncio pytest_asyncio pytest-mock
+    pip install debugpy aiohttp_basicauth asyncio pytest_asyncio pytest-mock types-PyYAML
     source_location="$PWD"/../kvmd
     rm -Rf ${usrsharekvmd_location}/web
     ln -s ${source_location}/web "${usrsharekvmd_location}/web"
