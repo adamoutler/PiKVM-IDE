f39e0ec14f8b8c13538010918aaa25bc9c4b8606
Adam Outler <adamoutler@gmail.com>
2024-06-08 15:12:45 +0000

    Update python version

--- .vscode/helpers/link-code-to-system.sh
+++ .vscode/helpers/link-code-to-system.sh
@@ -1,11 +1,11 @@
 sudo systemctl stop kvmd.service
-if [[ "$(python --version)" != "Python 3.11"* ]] ; then 
+if [[ "$(python --version)" != "Python 3.12"* ]] ; then 
     echo "Python version changed!  Please adjust link-code-to-system.sh"; 
     exit 1
 fi
 rw
 loaded=false
-kvmd_location=/usr/lib/python3.11/site-packages/kvmd
+kvmd_location=/usr/lib/python3.12/site-packages/kvmd
 usrsharekvmd_location=/usr/share/kvmd/
 test -L "${kvmd_location}" && loaded=true
 if ! ${loaded}; then
